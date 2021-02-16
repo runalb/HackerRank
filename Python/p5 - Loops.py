2236053294cd7e80ed75fc23c36b129f6e22ed85
Runal <65166862+runalb@users.noreply.github.com>
2021-02-16 11:55:51 +0530

    Loops

---
+++ Python/p5 - Loops.py
@@ -0,0 +1,35 @@
+#Loops
+
+'''
+Task
+The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+
+Example
+
+The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+
+0
+1
+4
+Input Format
+
+The first and only line contains the integer, .
+
+Constraints
+
+
+Output Format
+
+Print  lines, one corresponding to each .
+
+Sample Input 0
+
+5
+'''
+
+
+
+n = int(input())
+
+for x in range(0, n):
+    print(x ** 2)
